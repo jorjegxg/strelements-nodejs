ee7950eef072249eed00081033b4fb5408a38744
Luța Gheorghe <yotrevorgxg@gmail.com>
2025-03-26 07:40:06 +0200

    fix: cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,8 @@
 import cors from 'cors';
+//TODO: change in production
 
 const corsOptions = {
-  origin: [`${process.env.FRONTEND_LOCAL_URL!}`, `${process.env.FRONTEND_URL!}`],
+  origin: '*',//[`${process.env.FRONTEND_LOCAL_URL!}`, `${process.env.FRONTEND_URL!}`],
 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
