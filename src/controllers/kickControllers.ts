dfd718f9c2130cfb97aa58cf76259687daa0d7aa
Luța Gheorghe <yotrevorgxg@gmail.com>
2025-03-27 11:19:27 +0200

    debug

--- src/controllers/kickControllers.ts
+++ src/controllers/kickControllers.ts
@@ -5,18 +5,27 @@ import { subscribeToEvents } from '../services/hooksService';
 const exchangeCode = async (req: Request, res) => {
   const { authorizationCode, codeVerifier } = req.body;
 
+  console.log("Eveniment Kick primit:", req.body);
+
   //TODO: foloseste zod aici
 
   if (!authorizationCode || !codeVerifier) {
     return res.status(400).send('Missing required parameters');
   }
 
+  console.log('Received authorization code:', authorizationCode);
+  console.log('Received code verifier:', codeVerifier);
+
   try {
     const authData = await exchangeAuthCode(authorizationCode, codeVerifier);
+
+    console.log('Received auth data:', authData);
     await subscribeToEvents(authData.data.access_token);
 
+    console.log('Subscribed to events');
     res.json(authData);
   } catch (error: any) {
+    console.error('Error exchanging code:', error.message);
     res.status(500).send(error.message);
   }
 };
