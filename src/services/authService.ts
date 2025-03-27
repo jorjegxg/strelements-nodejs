8c7eb402ecc537bb7675384944f24329d5084886
Luța Gheorghe <yotrevorgxg@gmail.com>
2025-03-27 11:28:23 +0200

    fix : auth request

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,23 +2,26 @@ import axios from "axios";
 import { CONFIG } from "../config/config";
 
 async function exchangeAuthCode(authorizationCode: string, codeVerifier: string) {
-  try {
-    const response = await axios.post(CONFIG.KICK_AUTH_URL,
-      {
-        'code': authorizationCode,
-        'client_id': CONFIG.CLIENT_ID,
-        'client_secret': CONFIG.CLIENT_SECRET,
-        'redirect_uri': CONFIG.FRONTEND_URL + '/callback',
-        'grant_type': 'authorization_code',
-        'code_verifier': codeVerifier,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
 
-    );
+  const params = new URLSearchParams({
+    code: authorizationCode,
+    client_id: CONFIG.CLIENT_ID,
+    client_secret: CONFIG.CLIENT_SECRET,
+    redirect_uri: CONFIG.FRONTEND_URL + '/callback',
+    grant_type: 'authorization_code',
+    code_verifier: codeVerifier,
+  });
+
+
+  try {
+    console.log('Received authorization code:', authorizationCode);
+
+    const response = await axios.post(CONFIG.KICK_AUTH_URL, params, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    });
+
+
+    console.log('Received auth data:', response.data);
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.error_description || 'Failed to exchange authorization code');
