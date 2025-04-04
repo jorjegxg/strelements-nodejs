ec44e0e6e0323c0e8c52e9d418fc968ffbaacc8d
Luta Gheorghe <yotrevorgxg@gmail.com>
2025-04-04 22:27:37 +0300

    test , nu mai merge backendul deloc

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,21 @@
-require("dotenv").config();
-import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
-import listEndpoints from "express-list-endpoints";
-import { handleSubscribe, handleWebhook } from "./controllers/hooksController";
-import { exchangeCode } from "./controllers/kickControllers";
-import corsMiddleware from "./middleware/cors";
-import { requestLogger } from "./middleware/logger";
-import { validateRequest } from "./middleware/validation";
-import { exchangeCodeSchema } from "./models/exchangeCodeSchema";
-import { toggleRequestBodySchema } from "./models/toogleRequestSchema";
+import { Request, Response } from "express";
+
+import http from 'http';
+
+import express from 'express';
+
+
 
 const app = express();
+const server = http.createServer(app);
 
-//middleware for logging
-app.use(requestLogger);
+app.use(express.json());
 
-app.use(corsMiddleware);
-app.use(bodyParser.json());
-const port = process.env.PORT || 3000;
-//////////////////////////////////////////
-// app.use('/kick', kickRouter);
-app.post(
-  "/kick/login/exchange-code",
-  validateRequest(exchangeCodeSchema),
-  exchangeCode
-);
-app.post("/kick/hooks", handleWebhook);
-app.post("/toggle", validateRequest(toggleRequestBodySchema), handleSubscribe);
-
-//////////////////////////////////////////
-app.use(requestLogger);
-console.log(listEndpoints(app));
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Root!" });
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Backend server at http://localhost:${port}`);
+
+server.listen(3000, () => {
+  console.log('Serverul rulează pe portul 3000');
 });
