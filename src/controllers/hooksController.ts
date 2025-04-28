624d6ff7ec8342371f2fb9f45a730531bcf7e65c
Luta Gheorghe <yotrevorgxg@gmail.com>
2025-04-28 14:52:32 +0300

    debug events

--- src/controllers/hooksController.ts
+++ src/controllers/hooksController.ts
@@ -26,30 +26,24 @@ const handleWebhook = (req: Request, res: Response, io: Server) => {
   const { body , headers } = req;
   console.log("Received webhook headers:",  headers);
   console.log("Webhook received---------------------------:", body);
-  const user_id = body.broadcaster.user_id;
-  console.log("User ID -----------------------------xxxxxxx---------------------:", user_id);
-
+  
   if(headers["kick-event-type"] === "livestream.status.updated") {
-    console.log("Livestream status updated event received");
+    const user_id = body.broadcaster.user_id;
+    console.log("User ID -----------------------------xxxxxxx---------------------:", user_id);
 
-    console.log("Emitting live to room:", user_id);
+    
     io.to(`${user_id}`).emit("live", {
       headers: headers,
       body: body,
     }); 
-
-    //SEE HOW MANY PEOPLE ARE SUBSCRIBED TO SOKET
-
-    const sockets = io.sockets.adapter.rooms.get(user_id);
-    if (sockets) {
-      console.log(`Number of sockets connected to room ${user_id}: ${sockets.size}`);
-    }
-
     
-
-
   }else if(headers["kick-event-type"] === "chat.message.sent") {
-    io.to(user_id).emit("chat", {
+    const user_id = body.broadcaster.user_id;
+    console.log("User ID -----------------------------xxxxxxx---------------------:", user_id);
+    console.log("Chat message sent event received:", body);
+
+
+    io.to(`${user_id}`).emit("chat", {
       headers: headers,
       body: body,
     });
